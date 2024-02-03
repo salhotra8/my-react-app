9e04164c57377277ad3e77a663798414aebe4792
Himanshu Salhotra <himanshu_sa@blr.trigent.com>
2024-02-03 19:10:53 +0530

    code refactor

--- src/components/GameGrid/GameGrid.tsx
+++ src/components/GameGrid/GameGrid.tsx
@@ -1,42 +1,25 @@
 import { Container, Grid, Typography } from "@mui/material";
 import styles from "./gameGrid.module.scss";
-import { useEffect, useState } from "react";
-import axios from "../../services/game-client-api";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface GamesResposne {
-  count: string;
-  results: Game[];
-}
+import useGames from "../../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState({});
-
-  useEffect(() => {
-    axios
-      .get<GamesResposne>("/games")
-      .then((res) => {
-        setGames(res.data.results);
-      })
-      .catch((error) => {
-        setError(error);
-      });
-  });
+  const { games, error } = useGames();
 
   return (
     <Container className={styles.main}>
-    <Grid container rowSpacing={12} spacing={{ xs: 2, md: 3  }} columns={{ xs: 5, sm: 8, md: 12 }}>
-      {games.map((game: Game) => (
-        <Grid item xs={2} sm={4} md={4} key={game.id}>
-          <Typography>{game.name}</Typography>
-        </Grid>
-      ))}
-    </Grid>
+      {error && <Typography>{error}</Typography>}
+      <Grid
+        container
+        rowSpacing={12}
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 5, sm: 8, md: 12 }}
+      >
+        {games.map((game) => (
+          <Grid item xs={2} sm={4} md={4} key={game.id}>
+            <Typography>{game.name}</Typography>
+          </Grid>
+        ))}
+      </Grid>
     </Container>
   );
 };
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,35 @@
+import { AxiosResponse, AxiosError, CanceledError } from "axios";
+import { useEffect, useState } from "react";
+import gameClientApi from "../services/game-client-api";
+
+export interface Games {
+  id: number;
+  name: string;
+}
+
+export interface GamesResponse {
+  count: string;
+  results: Games[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Games[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    gameClientApi
+      .get<GamesResponse>("/games", { signal: controller.signal })
+      .then((res: AxiosResponse) => setGames(res.data.results))
+      .catch((error: AxiosError) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+      });
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
